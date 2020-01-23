157797f876e9ff60fa7a145837634bf7621ae97b
Technote <technote.space@gmail.com>
2020-01-23 14:10:03 +0900

    chore: tweaks

--- src/utils.ts
+++ src/utils.ts
@@ -6,16 +6,16 @@ import { NOT_FOUND_STATUS } from './constant';
 
 export const getConfig = async(fileName: string, octokit: GitHub, context: Context, configPath = '.github'): Promise<object | false> => {
 	try {
-		const contents = await octokit.repos.getContents({
+		return parseConfig((await octokit.repos.getContents({
 			owner: context.repo.owner,
 			repo: context.repo.repo,
 			path: path.posix.join(configPath, fileName),
-		});
-		return parseConfig(contents.data['content']);
+		})).data['content']);
 	} catch (error) {
 		if (error.status && NOT_FOUND_STATUS === error.status) {
 			return false;
 		}
+
 		throw error;
 	}
 };
