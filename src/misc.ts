c77963b009d57701611f90a7c4ec796d3e58ef04
Technote <technote.space@gmail.com>
2020-03-03 21:37:50 +0900

    chore: check object key length

--- src/misc.ts
+++ src/misc.ts
@@ -1,3 +1,10 @@
 import yaml from 'js-yaml';
 
-export const parseConfig = (content: string): object | false => yaml.safeLoad(Buffer.from(content, 'base64').toString()) || false;
+export const parseConfig = (content: string): object | false => {
+	const config = yaml.safeLoad(Buffer.from(content, 'base64').toString());
+	if (!config || !Object.keys(config).length) {
+		return false;
+	}
+
+	return config;
+};
