bad2fbbcf6f1b5ebd38828a8ccdf60f9e07aaf54
Technote <technote.space@gmail.com>
2020-06-26 02:00:54 +0900

    feat: update library usage
    BREAKING CHANGE: change octokit type

--- src/utils.ts
+++ src/utils.ts
@@ -1,26 +1,32 @@
-import { join } from 'path';
-import { Octokit } from '@octokit/rest';
-import { Context } from '@actions/github/lib/context';
-import { parseConfig } from './misc';
-import { NOT_FOUND_STATUS } from './constant';
+import {join} from 'path';
+import {RestEndpointMethods} from '@octokit/plugin-rest-endpoint-methods/dist-types/generated/method-types';
+import {Context} from '@actions/github/lib/context';
+import {parseConfig} from './misc';
+import {NOT_FOUND_STATUS} from './constant';
 
-export const getConfig = async(fileName: string, octokit: Octokit, context: Context, options: { configPath?: string; ref?: string } = {}): Promise<object | false> | never => {
-	const {configPath = '.github', ref} = options;
-	try {
-		const params: Octokit.ReposGetContentsParams = {
-			owner: context.repo.owner,
-			repo: context.repo.repo,
-			path: join(configPath, fileName),
-		};
-		if (ref) {
-			params.ref = ref;
-		}
-		return parseConfig((await octokit.repos.getContents(params)).data['content']);
-	} catch (error) {
-		if (error.status && NOT_FOUND_STATUS === error.status) {
-			return false;
-		}
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const getConfig = async(fileName: string, octokit: RestEndpointMethods, context: Context, options: { configPath?: string; ref?: string } = {}): Promise<{ [key: string]: any } | false> | never => {
+  const {configPath = '.github', ref} = options;
+  try {
+    const params: {
+      owner: string;
+      repo: string;
+      path: string;
+      ref?: string;
+    } = {
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      path: join(configPath, fileName),
+    };
+    if (ref) {
+      params.ref = ref;
+    }
+    return parseConfig((await octokit.repos.getContent(params)).data['content']);
+  } catch (error) {
+    if (error.status && NOT_FOUND_STATUS === error.status) {
+      return false;
+    }
 
-		throw error;
-	}
+    throw error;
+  }
 };
