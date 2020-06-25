20eb22c43ff1f86def8b83f33beae6217bea505e
Technote <technote.space@gmail.com>
2020-06-26 03:22:44 +0900

    chore: tweaks

--- src/utils.ts
+++ src/utils.ts
@@ -1,11 +1,12 @@
 import {join} from 'path';
-import {RestEndpointMethods} from '@octokit/plugin-rest-endpoint-methods/dist-types/generated/method-types';
 import {Context} from '@actions/github/lib/context';
+import {Octokit} from '@technote-space/github-action-helper/dist/types';
+import {RestEndpointMethods} from '@octokit/plugin-rest-endpoint-methods/dist-types/generated/method-types';
 import {parseConfig} from './misc';
 import {NOT_FOUND_STATUS} from './constant';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const getConfig = async(fileName: string, octokit: RestEndpointMethods, context: Context, options: { configPath?: string; ref?: string } = {}): Promise<{ [key: string]: any } | false> | never => {
+export const getConfig = async(fileName: string, octokit: Octokit, context: Context, options: { configPath?: string; ref?: string } = {}): Promise<{ [key: string]: any } | false> | never => {
   const {configPath = '.github', ref} = options;
   try {
     const params: {
@@ -21,7 +22,7 @@ export const getConfig = async(fileName: string, octokit: RestEndpointMethods, c
     if (ref) {
       params.ref = ref;
     }
-    return parseConfig((await octokit.repos.getContent(params)).data['content']);
+    return parseConfig((await (octokit as RestEndpointMethods).repos.getContent(params)).data['content']);
   } catch (error) {
     if (error.status && NOT_FOUND_STATUS === error.status) {
       return false;
