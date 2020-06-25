8e6bd9912ea2895de45a674beba77cd33c6bb5e1
Technote <technote.space@gmail.com>
2020-06-26 01:59:21 +0900

    chore: code formattings

--- __tests__/misc.test.ts
+++ __tests__/misc.test.ts
@@ -1,78 +1,78 @@
 /* eslint-disable no-magic-numbers */
-import { encodeContent, spyOnStdout, stdoutCalledWith, getLogStdout } from '@technote-space/github-action-test-helper';
-import { parseConfig } from '../src';
-import { isYaml, parseYamlConfig, parseJsonConfig } from '../src/misc';
+import {encodeContent, spyOnStdout, stdoutCalledWith, getLogStdout} from '@technote-space/github-action-test-helper';
+import {parseConfig} from '../src';
+import {isYaml, parseYamlConfig, parseJsonConfig} from '../src/misc';
 
 describe('isYaml', () => {
-	it('should return true', () => {
-		expect(isYaml('test.yaml')).toBe(true);
-		expect(isYaml('/tmp/test.yml')).toBe(true);
-		expect(isYaml('.yml')).toBe(true);
-	});
+  it('should return true', () => {
+    expect(isYaml('test.yaml')).toBe(true);
+    expect(isYaml('/tmp/test.yml')).toBe(true);
+    expect(isYaml('.yml')).toBe(true);
+  });
 
-	it('should return false', () => {
-		expect(isYaml('')).toBe(false);
-		expect(isYaml('.eslintrc')).toBe(false);
-		expect(isYaml('LICENSE')).toBe(false);
-		expect(isYaml('/tmp/test.txt')).toBe(false);
-	});
+  it('should return false', () => {
+    expect(isYaml('')).toBe(false);
+    expect(isYaml('.eslintrc')).toBe(false);
+    expect(isYaml('LICENSE')).toBe(false);
+    expect(isYaml('/tmp/test.txt')).toBe(false);
+  });
 });
 
 describe('parseYamlConfig', () => {
-	it('should parse config', async() => {
-		expect(parseYamlConfig('a: b')).toEqual({a: 'b'});
-		expect(parseYamlConfig('a:\n  - b\n  - c')).toEqual({a: ['b', 'c']});
-	});
+  it('should parse config', async() => {
+    expect(parseYamlConfig('a: b')).toEqual({a: 'b'});
+    expect(parseYamlConfig('a:\n  - b\n  - c')).toEqual({a: ['b', 'c']});
+  });
 
-	it('should return empty if empty', () => {
-		expect(parseYamlConfig('')).toEqual({});
-	});
+  it('should return empty if empty', () => {
+    expect(parseYamlConfig('')).toEqual({});
+  });
 
-	it('should return empty if error', () => {
-		const mockStdout = spyOnStdout();
-		expect(parseYamlConfig('Test1\nTest2: Test3')).toEqual({});
-		stdoutCalledWith(mockStdout, [
-			getLogStdout({
-				name: 'YAMLException',
-				reason: 'end of the stream or a document separator is expected',
-				mark: {
-					name: null,
-					buffer: 'Test1\nTest2: Test3\n\u0000',
-					position: 11,
-					line: 1,
-					column: 5,
-				},
-				message: 'end of the stream or a document separator is expected at line 2, column 6:\n    Test2: Test3\n         ^',
-			}, '__error__'),
-		]);
-	});
+  it('should return empty if error', () => {
+    const mockStdout = spyOnStdout();
+    expect(parseYamlConfig('Test1\nTest2: Test3')).toEqual({});
+    stdoutCalledWith(mockStdout, [
+      getLogStdout({
+        name: 'YAMLException',
+        reason: 'end of the stream or a document separator is expected',
+        mark: {
+          name: null,
+          buffer: 'Test1\nTest2: Test3\n\u0000',
+          position: 11,
+          line: 1,
+          column: 5,
+        },
+        message: 'end of the stream or a document separator is expected at line 2, column 6:\n    Test2: Test3\n         ^',
+      }, '__error__'),
+    ]);
+  });
 });
 
 describe('parseJsonConfig', () => {
-	it('should parse config', async() => {
-		expect(parseJsonConfig('{"a": "b"}')).toEqual({a: 'b'});
-		expect(parseJsonConfig('{"a": ["b", "c"]}')).toEqual({a: ['b', 'c']});
-	});
+  it('should parse config', async() => {
+    expect(parseJsonConfig('{"a": "b"}')).toEqual({a: 'b'});
+    expect(parseJsonConfig('{"a": ["b", "c"]}')).toEqual({a: ['b', 'c']});
+  });
 
-	it('should return empty if error', () => {
-		const mockStdout = spyOnStdout();
-		expect(parseJsonConfig('')).toEqual({});
-		expect(parseJsonConfig('[1, 2,]')).toEqual({});
-		stdoutCalledWith(mockStdout, [
-			getLogStdout({}, '__error__'),
-			getLogStdout({}, '__error__'),
-		]);
-	});
+  it('should return empty if error', () => {
+    const mockStdout = spyOnStdout();
+    expect(parseJsonConfig('')).toEqual({});
+    expect(parseJsonConfig('[1, 2,]')).toEqual({});
+    stdoutCalledWith(mockStdout, [
+      getLogStdout({}, '__error__'),
+      getLogStdout({}, '__error__'),
+    ]);
+  });
 });
 
 describe('parseConfig', () => {
-	it('should parse yaml', async() => {
-		expect(parseConfig(encodeContent('a: b'))).toEqual({a: 'b'});
-		expect(parseConfig(encodeContent('a: b'), 'test.yaml')).toEqual({a: 'b'});
-	});
+  it('should parse yaml', async() => {
+    expect(parseConfig(encodeContent('a: b'))).toEqual({a: 'b'});
+    expect(parseConfig(encodeContent('a: b'), 'test.yaml')).toEqual({a: 'b'});
+  });
 
-	it('should parse json', () => {
-		expect(parseConfig(encodeContent('{"a": "b"}'), 'test.json')).toEqual({a: 'b'});
-		expect(parseConfig(encodeContent('{"a": "b"}'), '.eslintrc')).toEqual({a: 'b'});
-	});
+  it('should parse json', () => {
+    expect(parseConfig(encodeContent('{"a": "b"}'), 'test.json')).toEqual({a: 'b'});
+    expect(parseConfig(encodeContent('{"a": "b"}'), '.eslintrc')).toEqual({a: 'b'});
+  });
 });
--- src/misc.ts
+++ src/misc.ts
@@ -2,29 +2,32 @@ import yaml from 'js-yaml';
 
 export const isYaml = (filename: string): boolean => ['yaml', 'yml'].includes(String(filename.split('.').pop()));
 
-export const parseYamlConfig = (content: string): object => {
-	try {
-		const config = yaml.safeLoad(content);
-		if (!config || !Object.keys(config).length) {
-			return {};
-		}
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const parseYamlConfig = (content: string): { [key: string]: any } => {
+  try {
+    const config = yaml.safeLoad(content);
+    if (!config || !Object.keys(config).length) {
+      return {};
+    }
 
-		return config;
-	} catch (error) {
-		console.error(error);
-		return {};
-	}
+    return config;
+  } catch (error) {
+    console.error(error);
+    return {};
+  }
 };
 
-export const parseJsonConfig = (content: string): object => {
-	try {
-		return JSON.parse(content);
-	} catch (error) {
-		console.error(error);
-		return {};
-	}
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const parseJsonConfig = (content: string): { [key: string]: any } => {
+  try {
+    return JSON.parse(content);
+  } catch (error) {
+    console.error(error);
+    return {};
+  }
 };
 
-export const parseConfig = (content: string, filename?: string): object => undefined === filename || isYaml(filename) ?
-	parseYamlConfig(Buffer.from(content, 'base64').toString()) :
-	parseJsonConfig(Buffer.from(content, 'base64').toString());
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const parseConfig = (content: string, filename?: string): { [key: string]: any } => undefined === filename || isYaml(filename) ?
+  parseYamlConfig(Buffer.from(content, 'base64').toString()) :
+  parseJsonConfig(Buffer.from(content, 'base64').toString());
